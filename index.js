6d40ef6e70dc89b2af88b5c0e39428bc193035bf
jlmm0021 <jlmm0021@red.ujaen.es>
2022-12-08 23:02:41 +0100

    Carga inicial terminada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const fs = require('fs')
 
 const PORT = 80;
 const app = express();
@@ -9,18 +9,16 @@ const app = express();
 app.use('/css', express.static(path.join(__dirname + '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname + '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname + '/js')));
+app.use('/data', express.static(path.join(__dirname + '/data')));
 
 app.get('/hola', function(req,res){
     res.send("Hola!");
 });
 
-app.get("/", inicio);
-
-function inicio (peticion, resultado)
-{
-   resultado.sendFile(__dirname + "/html/index.html");
-}
-
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/html/index.html");
+});
 
 app.listen(PORT)
 console.log(`Server iniciado en el puerto ${PORT}`);
+
--- js/front.js
+++ js/front.js
@@ -1,23 +1,33 @@
-
-
-
 function carga() {
+  let datos;
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "/data/data.json", true);
+  xhttp.send();
 
-var contendor  = $("#tbody").html();
- var nuevaFila = '<tr>';
- nuevaFila  += '<th scope="row">4</th>';
- nuevaFila  += ('<td>' + ahora() + '</td>');
- nuevaFila  += '<td>"Permitido"</td>';
- nuevaFila  += '</tr>';
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      datos = JSON.parse(this.responseText);
+      console.log(datos);
 
- $(nuevaFila).appendTo("#tbody");
-
+      for (let i = 0; i < datos.entradas.length; i++) {
+        var contendor = $("#tbody").html();
+        var nuevaFila = "<tr>";
+        nuevaFila += '<th scope="row">4</th>';
+        nuevaFila += "<td>" + ahora() + "</td>";
+        nuevaFila += "<td>" + datos.entradas[i].fecha + "</td>";
+        nuevaFila += "</tr>";
+        $(nuevaFila).appendTo("#tbody");
+      }
+    }
+  };
 }
 
 function ahora() {
-    var hoy = new Date();
-    var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear();
-    var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
-    var fechaYHora = fecha + ' ' + hora;
-    return fechaYHora;
+  var hoy = new Date();
+  var fecha =
+    hoy.getDate() + "-" + (hoy.getMonth() + 1) + "-" + hoy.getFullYear();
+  var hora = hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds();
+  var fechaYHora = fecha + " " + hora;
+  return fechaYHora;
 }
